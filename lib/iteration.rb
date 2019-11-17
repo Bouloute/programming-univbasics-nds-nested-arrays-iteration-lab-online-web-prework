da44d43163e67898225149f3ff999aff65e8223b
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-17 03:03:53 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,8 +6,7 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-  i, j = 0 
-  for( i = 0)
+  
   #joined_ingredients[`I love $[src[O]] and $[src[1]] on my pizza` ]
 end
 
